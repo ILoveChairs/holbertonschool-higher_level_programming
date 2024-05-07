c05ed9e577f4aa137e0f503c30aa6ef30637113e
Matias Davezac <8675@holbertonstudents.com>
2024-05-07 10:48:30 -0300

    Fixed T0, added to run only if main.

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -2,7 +2,9 @@
 
 from add_0 import add
 
-num1 = 1
-num2 = 2
-result = add(1, 2)
-print(f"{num1} + {num2} = {result}")
+
+if __name__ == "__main__":
+    num1 = 1
+    num2 = 2
+    result = add(1, 2)
+    print(f"{num1} + {num2} = {result}")
