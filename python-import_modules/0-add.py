cc58b15700682c56c08d77ae5e5c5554048599d2
Matias Davezac <8675@holbertonstudents.com>
2024-05-07 10:54:45 -0300

    Fixing T0.

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -4,7 +4,6 @@ from add_0 import add
 
 
 if __name__ == "__main__":
-    num1 = 1
-    num2 = 2
-    result = add(1, 2)
-    print("{} + {} = {}".format(num1, num2, result))
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
