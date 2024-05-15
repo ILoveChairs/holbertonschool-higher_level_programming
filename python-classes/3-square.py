cb6181ff7097d45171d4ef916bfe526fbcbf7ce6
Matias Davezac <8675@holbertonstudents.com>
2024-05-15 15:46:25 -0300

    Fixed T3, Idk

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -18,7 +18,6 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-    
-    @property
+
     def area(self):
         return self.__size ** 2
