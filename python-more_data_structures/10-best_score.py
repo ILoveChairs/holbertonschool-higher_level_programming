42c26e02206e2b83a54b51654300ef6a6e880e73
Matias Davezac <8675@holbertonstudents.com>
2024-05-15 12:02:29 -0300

    Fixed T10, pep8, None comparison fix

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if a_dictionary == None or len(a_dictionary) == 0:
+    if a_dictionary is None or len(a_dictionary) == 0:
         return None
     max_num_key = None
     max_num = None
     for key in a_dictionary:
-        if max_num == None or a_dictionary[key] > max_num:
+        if max_num is None or a_dictionary[key] > max_num:
             max_num_key = key
             max_num = a_dictionary[key]
     return max_num_key
