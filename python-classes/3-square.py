2e0d688b777a2eb1ae0bfde10831f36f89e407c1
Matias Davezac <8675@holbertonstudents.com>
2024-05-15 15:42:50 -0300

    Added T3

---
+++ python-classes/3-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+'''Square:
+    gil
+'''
+
+
+class Square:
+    '''Does square
+
+    Attributes:
+        __size (int): Size of square
+    '''
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    
+    def area(self):
+        return self.__size ** 2
