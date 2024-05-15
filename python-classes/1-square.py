cd316e955231d24ccb01098779cae594ffb987e3
Matias Davezac <8675@holbertonstudents.com>
2024-05-15 15:35:06 -0300

    Added T1

---
+++ python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+'''Square:
+    gil
+'''
+
+
+class Square:
+    '''Does square
+
+    Attributes:
+        __size (int): Size of square
+    '''
+
+    def __init__(self, size):
+        self.__size = size
