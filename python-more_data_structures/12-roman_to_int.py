01cbfa16aeae3c6fe615e28283a6be0826762e30
Matias Davezac <8675@holbertonstudents.com>
2024-05-15 13:54:08 -0300

    Fixed T12, Now reruns get max every char

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 
 def get_value_from_roman_char(roman_char):
-    roman_dict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    roman_dict = {
+        "I": 1, "V": 5,
+        "X": 10, "L": 50,
+        "C": 100, "D": 500,
+        "M": 1000
+        }
     return roman_dict[roman_char]
+
 
 def get_max_from_roman_str(roman_string):
     max_char = None
@@ -14,18 +20,16 @@ def get_max_from_roman_str(roman_string):
             max = value
     return max_char
 
+
 def roman_to_int(roman_string):
     if roman_string is None or not isinstance(roman_string, str):
         return 0
     sum = 0
-    max_char = get_max_from_roman_str(roman_string)
-    prior_max = True
-    for roman_char in roman_string:
-        if prior_max and roman_char == max_char:
-            prior_max = False
+    for i, roman_char in enumerate(roman_string):
+        max_char = get_max_from_roman_str(roman_string[i:])
         roman_char_value = get_value_from_roman_char(roman_char)
-        if prior_max:
-            sum -= roman_char_value
-        else:
+        if roman_char == max_char:
             sum += roman_char_value
+        else:
+            sum -= roman_char_value
     return sum
