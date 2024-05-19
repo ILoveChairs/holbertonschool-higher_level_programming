ce2d18ee5eaa6df071389fb7f4d983a1918edd6c
Matias Davezac <8675@holbertonstudents.com>
2024-05-19 18:29:39 +0000

    Added T102

---
+++ python-classes/102-square.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+
+'''Square:
+    gil
+'''
+
+
+class Square:
+    '''Does square
+
+    Attributes:
+        __size (int): Size of square
+    '''
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def __eq__(self, other):
+        return self.area() == other.area()
+
+    def __ne__(self, other):
+        return self.area() != other.area()
+
+    def __lt__(self, other):
+        return self.area() < other.area()
+
+    def __le__(self, other):
+        return self.area() <= other.area()
+
+    def __gt__(self, other):
+        return self.area() > other.area()
+
+    def __ge__(self, other):
+        return self.area() >= other.area()
