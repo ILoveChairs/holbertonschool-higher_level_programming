2852eb75b5d036be0013020e236b8923219fc81c
Matias Davezac <8675@holbertonstudents.com>
2024-05-20 11:18:11 +0000

    Added T0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python3
+'''
+    Write a function that adds 2 integers.
+'''
 
 
+def add_integer(a, b=98):
+    '''
+        Safely adds 2 integers
+    '''
+    a = int(a) if isinstance(a, float) else a
+    b = int(b) if isinstance(b, float) else b
+    if not isinstance(a, int):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int):
+        raise TypeError("b must be an integer")
+    return a + b
