be8d4dd99f8ed86ae7d1b41b067c23598521fe01
Matias Davezac <8675@holbertonstudents.com>
2024-05-21 11:01:53 -0300

    Added T4

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,3 +1,20 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+
+'''
+    Duck
+'''
 
 
+def print_square(size):
+    '''
+        Goes quack quack
+    '''
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    
+    for row in range(size):
+        for element in range(size):
+            print("#", end="")
+        print("")
