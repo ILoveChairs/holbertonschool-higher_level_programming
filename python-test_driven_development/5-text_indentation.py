0591d4df88f0b1843cebb3555981eac34a24ae75
Matias Davezac <8675@holbertonstudents.com>
2024-05-21 11:26:35 -0300

    Added T5

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,3 +1,24 @@
 #!/usr/bin/env python3
 
+'''
+    Duck
+'''
 
+
+def text_indentation(text):
+    '''
+        Goes quack quack
+    '''
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    flag = False
+    for char in text:
+        if flag and char == " ":
+            pass
+        elif char not in ".:?":
+            print(char, end="")
+            flag = False
+        else:
+            print(char, end="\n\n")
+            flag = True
