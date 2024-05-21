699709e0093d29e5044db93cb39204d4adfce65b
Matias Davezac <8675@holbertonstudents.com>
2024-05-21 14:03:16 -0300

    Added T2

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -1,3 +1,48 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+
+'''
+    Defines rectangle class.
+'''
 
 
+class Rectangle:
+    '''
+        Rectangle class.
+
+        height(int, >=0): Height of rectangle.
+        width(int, >=0): Width of rectangle.
+    '''
+
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return (self.width * 2) + (self.height * 2)
