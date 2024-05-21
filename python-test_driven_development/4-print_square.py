622050414861e23705026fc3e0bb9ffff0cd1f65
Matias Davezac <8675@holbertonstudents.com>
2024-05-21 11:02:38 -0300

    Fixed T4, fixed pep8

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -13,7 +13,7 @@ def print_square(size):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-    
+
     for row in range(size):
         for element in range(size):
             print("#", end="")
