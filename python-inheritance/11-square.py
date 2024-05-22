6c95cf9c8ebfcfa16e5eefacd17000a8b52f857c
Matias Davezac <8675@holbertonstudents.com>
2024-05-22 14:59:06 -0300

    Fixed T11, idk

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -43,8 +43,9 @@ class Square(Rectangle):
     '''
 
     def __init__(self, size):
-        super().__init__(size, size)
+        self.integer_validator("size", size)
         self.__size = size
+        super().__init__(size, size)
 
     def __str__(self):
         return f"[Square] {self.__size}/{self.__size}"
