93e27c2741b82d3d6f20ac4c0eb677950b518d7d
Matias Davezac <8675@holbertonstudents.com>
2024-05-22 10:43:47 -0300

    Added T2

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,3 +1,12 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+
+'''
+    DGQQ
+'''
 
 
+def is_same_class(obj, a_class):
+    '''
+        Duck Goes Quack Quack
+    '''
+    return obj.__class__.__name__ == a_class.__name__
