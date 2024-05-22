2eebfa4740fca1498e9a34661600a17e22879a68
Matias Davezac <8675@holbertonstudents.com>
2024-05-22 11:09:37 -0300

    Added T4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,3 +1,13 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+
+'''
+    DGQQ
+'''
 
 
+def inherits_from(obj, a_class):
+    '''
+        Duck Goes Quack Quack
+    '''
+    return a_class in obj.__class__.__mro__ and not \
+        obj.__class__.__name__ == a_class.__name__
