43dec5feb5e64dd5a206c1a6a5f0697a3e2ea618
Matias Davezac <8675@holbertonstudents.com>
2024-05-22 11:24:00 -0300

    Added T7

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,3 +1,19 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+
+'''
+    DGQQ
+'''
 
 
+class BaseGeometry:
+    '''
+        Duck Goes Quack Quack
+    '''
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
