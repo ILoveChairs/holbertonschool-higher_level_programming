8d2d48a9e90708e8dd76399f71faccf073dffab2
Matias Davezac <8675@holbertonstudents.com>
2024-05-22 11:00:24 -0300

    Added T3

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,3 +1,12 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+'''
+    DGQQ
+'''
 
 
+def is_kind_of_class(obj, a_class):
+    '''
+        Duck Goes Quack Quack
+    '''
+    return a_class in obj.__class__.__bases__ or \
+        obj.__class__.__name__ == a_class.__name__
