9d1c0c9b2ecc8633565a7b222c1c6bf96a8e16c6
Matias Davezac <8675@holbertonstudents.com>
2024-05-22 11:15:44 -0300

    Added T6

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,3 +1,13 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+
+'''
+    DGQQ
+'''
 
 
+class BaseGeometry:
+    '''
+        Duck Goes Quack Quack
+    '''
+    def area(self):
+        raise Exception("area() is not implemented")
