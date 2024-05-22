856558dbf522dbcf5db1677e40d8751ae4496633
Matias Davezac <8675@holbertonstudents.com>
2024-05-22 14:44:35 -0300

    Added T10

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,3 +1,42 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+
+'''
+    DGQQ
+'''
 
 
+class BaseGeometry:
+    '''
+        Duck Goes Quack Quack
+    '''
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if value.__class__.__name__ != 'int':
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    '''
+        Rich square
+    '''
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
+
+
+class Square(Rectangle):
+    def __init__(self, size):
+        super().__init__(size, size)
