bf9df4b8ef6616b874c7837d9b337d78b289f022
Matias Davezac <8675@holbertonstudents.com>
2024-05-23 12:13:25 -0300

    Added T3

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -1,3 +1,26 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+
+'''
+    Counting iterator object
+'''
 
 
+class CountedIterator():
+    '''
+        CountedIterator
+    '''
+
+    def __init__(self, data):
+        self.__iter = iter(data)
+        self.__counter = 0
+        self.__len = self.__iter.__length_hint__()
+
+    def get_count(self):
+        return self.__counter
+
+    def __next__(self):
+        if self.__len <= self.__counter:
+            raise StopIteration()
+        self.__counter += 1
+        # return self.__iter.__getitem__(self.__counter)
+        return self.__iter.__next__()
