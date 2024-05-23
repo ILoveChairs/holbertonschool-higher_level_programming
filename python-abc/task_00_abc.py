dba617762cb3936aa3490f56e28edfe6118770fc
Matias Davezac <8675@holbertonstudents.com>
2024-05-23 10:23:53 -0300

    Added T0

--- python-abc/task_00_abc.py
+++ python-abc/task_00_abc.py
@@ -1,3 +1,36 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+
+'''
+    Using ABC
+'''
+
+from abc import ABC, abstractmethod
 
 
+class Animal(ABC):
+    '''
+        Animal
+    '''
+
+    
+    @abstractmethod
+    def sound(self):
+        pass
+
+
+class Dog(Animal):
+    '''
+        Dog
+    '''
+
+    def sound(self):
+        return "Bark"
+
+
+class Cat(Animal):
+    '''
+        Cat
+    '''
+
+    def sound(self):
+        return "Meow"
