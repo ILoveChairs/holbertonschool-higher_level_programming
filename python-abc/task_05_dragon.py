7914f5bb9bb34de56c09bf0e9ae728ce3c30ae4d
Matias Davezac <8675@holbertonstudents.com>
2024-05-23 13:00:31 -0300

    Added T5

--- python-abc/task_05_dragon.py
+++ python-abc/task_05_dragon.py
@@ -1,3 +1,32 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+
+'''
+    Fish flying
+'''
 
 
+class SwimMixin():
+    '''
+        Mixin to swim
+    '''
+
+    def swim(self):
+        print("The creature swims!")
+
+
+class FlyMixin():
+    '''
+        Mixin to fly
+    '''
+
+    def fly(self):
+        print("The creature flies!")
+
+
+class Dragon(SwimMixin, FlyMixin):
+    '''
+        Dragon <- swim, fly
+    '''
+
+    def roar(self):
+        print("The dragon roars!")
