13c1d49d2b7908ba5674d3dadaa629dda9cba777
Matias Davezac <8675@holbertonstudents.com>
2024-05-23 12:22:06 -0300

    Added T4

--- python-abc/task_04_flyingfish.py
+++ python-abc/task_04_flyingfish.py
@@ -1,3 +1,44 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+
+'''
+    Fish flying
+'''
 
 
+class Fish():
+    '''
+        Fish
+    '''
+
+    def swim(self):
+        print("The fish is swimming")
+
+    def habitat(self):
+        print("The fish lives in water")
+
+
+class Bird():
+    '''
+        Bird
+    '''
+
+    def fly(self):
+        print("The bird is flying")
+
+    def habitat(self):
+        print("The bird lives in the sky")
+
+
+class FlyingFish(Fish, Bird):
+    '''
+        FlyingFish
+    '''
+
+    def swim(self):
+        print("The flying fish is swimming")
+
+    def fly(self):
+        print("The flying fish is flying")
+
+    def habitat(self):
+        print("The flying fish lives both in water and the sky!")
