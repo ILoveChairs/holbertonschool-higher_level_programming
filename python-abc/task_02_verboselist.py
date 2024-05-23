6374e248ced4028d33498748f1307f4d76e013e2
Matias Davezac <8675@holbertonstudents.com>
2024-05-23 11:19:52 -0300

    Added T2

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -1,3 +1,30 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+
+'''
+    Making our own list from list
+'''
+
+from typing import Iterable, SupportsIndex
 
 
+class VerboseList(list):
+    '''
+        VerboseList <- list
+    '''
+
+    def append(self, __object) -> None:
+        result = super().append(__object)
+        print(f"Added [{__object}] to the list.")
+        return result
+
+    def extend(self, __iterable: Iterable) -> None:
+        result = super().extend(__iterable)
+        print(f"Extended the list with [{len(__iterable)}] items")
+        return result
+
+    def remove(self, __value) -> None:
+        print(f"Removed [{__value}] from the list")
+        return super().remove(__value)
+    def pop(self, __index: SupportsIndex = -1):
+        print(f"Popped [{self.__getitem__(__index)}] from the list.")
+        return super().pop(__index)
