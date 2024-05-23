25d2293dc0993045a18e2c4c8049b23bcd6e855c
Matias Davezac <8675@holbertonstudents.com>
2024-05-23 10:44:35 -0300

    Fixed T1, added incorrect input handling

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -31,6 +31,10 @@ class Circle(Shape):
     '''
 
     def __init__(self, radius):
+        if not isinstance(radius, int):
+            raise TypeError("radius must be an int")
+        if radius < 0:
+            raise ValueError("radius must be >= 0")
         self.__radius = radius
 
     def area(self):
@@ -49,6 +53,14 @@ class Rectangle(Shape):
     '''
 
     def __init__(self, width, height):
+        if not isinstance(width, int):
+            raise TypeError("width must be an int")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if not isinstance(height, int):
+            raise TypeError("height must be an int")
+        if height < 0:
+            raise ValueError("height must be >= 0")
         self.__width = width
         self.__height = height
 
