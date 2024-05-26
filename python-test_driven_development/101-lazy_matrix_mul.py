e0d7a0e3c990b10e160f721f1729a553e33a2702
Matias Davezac <8675@holbertonstudents.com>
2024-05-26 20:50:17 +0000

    Fixed T101, added documentation.

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 
 '''
-    Defines matrix_mul
+    Defines lazy_matrix_mul
 '''
 
 from numpy import matmul
 
 
 def lazy_matrix_mul(m_a, m_b):
+    '''
+        Lazy matrix mul
+    '''
     return matmul(m_a, m_b)
