a3c04616bae7a225aba6c0fa2ac1b6e0fa42a6b4
Matias Davezac <8675@holbertonstudents.com>
2024-05-26 21:36:36 +0000

    Added T101.

---
+++ python-inheritance/101-add_attribute.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+'''
+    Adds add_attribute
+'''
+
+
+def add_attribute(cls, varname, value):
+    if not isinstance(cls, object) or not isinstance(varname, str):
+        raise TypeError("can't add new attribute")
+    if "__setattr__" not in dir(cls):
+        raise TypeError("can't add new attribute")
+    setattr(cls, varname, value)
