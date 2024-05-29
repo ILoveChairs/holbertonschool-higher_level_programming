391c44dbfd5533406dd2413712ba01842cafbc14
Matias Davezac <8675@holbertonstudents.com>
2024-05-29 14:26:15 -0300

    Fixed T0?

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -4,7 +4,7 @@
     quick doc
 '''
 
-import pickle
+import json
 
 
 def serialize_and_save_to_file(data, filename):
@@ -12,8 +12,8 @@ def serialize_and_save_to_file(data, filename):
         quick doc
     '''
 
-    with open(filename, "wb+") as f:
-        pickle.dump(data, f)
+    with open(filename, "w+") as f:
+        json.dump(data, f)
 
 
 def load_and_deserialize(filename):
@@ -22,6 +22,6 @@ def load_and_deserialize(filename):
     '''
 
     read = None
-    with open(filename, "rb") as f:
-        read = pickle.load(f)
+    with open(filename, "r") as f:
+        read = json.load(f)
     return read
