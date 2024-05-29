c1b16b86132c9c7791f0ebde710c56f8c770fb93
Matias Davezac <8675@holbertonstudents.com>
2024-05-29 10:15:31 -0300

    Added T1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
 
 '''
-	quick doc
+    quick doc
 '''
+
+
+def write_file(filename="", text=""):
+    '''
+        quick doc
+    '''
+
+    n_written = 0
+    with open(filename, "w+") as f:
+        n_written = f.write(text)
+    return n_written
