39c7372c94dbe82808192f028bf1cb3037a6010f
Matias Davezac <8675@holbertonstudents.com>
2024-05-29 11:14:36 -0300

    Fixed T7, pep8

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 
 '''
-	quick doc
+    quick doc
 '''
+
+from sys import argv
 
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
-from sys import argv
+
 
 if __name__ == "__main__":
     for arg in argv[1:]:
