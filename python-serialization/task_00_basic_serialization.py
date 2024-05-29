f42dd20ba535366f4a7f614af1a4c8bf819a3124
Matias Davezac <8675@holbertonstudents.com>
2024-05-29 13:56:14 -0300

    Added T0

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,5 +1,31 @@
 #!/usr/bin/python3
 
 '''
-	quick doc
+    quick doc
 '''
+
+import pickle
+
+
+def serialize_and_save_to_file(data, filename):
+    '''
+        quick doc
+    '''
+
+    output = pickle.dumps(data)
+
+    with open(filename, "w+") as f:
+        f.write(output)
+
+
+def load_and_deserialize(filename):
+    '''
+        quick doc
+    '''
+
+    data = None
+
+    with open(filename, "r") as f:
+        data = f
+
+    return pickle.loads(data)
