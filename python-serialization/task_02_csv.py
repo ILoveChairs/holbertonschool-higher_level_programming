bb9fbadea5baa42542b3ba37b61bf4423bc72b54
Matias Davezac <8675@holbertonstudents.com>
2024-05-29 15:19:07 -0300

    Added T2 wo format

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -1,5 +1,29 @@
 #!/usr/bin/python3
 
 '''
-	quick doc
+    quick doc
 '''
+
+import csv
+import json
+
+
+def convert_csv_to_json(filename):
+    '''
+        quick doc
+    '''
+
+    output = []
+
+    try:
+        with open(filename, "r", newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                output.append(row)
+    except Exception:
+        return False
+
+    with open("data.json", "w+") as f:
+        json.dump(output, f)
+
+    return True
