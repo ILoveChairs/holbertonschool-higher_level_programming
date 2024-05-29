a494f7095afc11b561a9e1369311585ad1e26a18
Matias Davezac <8675@holbertonstudents.com>
2024-05-29 14:35:58 -0300

    Added T1

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -1,5 +1,34 @@
 #!/usr/bin/python3
 
 '''
-	quick doc
+    quick doc
 '''
+
+import pickle
+
+
+class CustomObject:
+    '''
+        quick doc
+    '''
+
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Is Student: {self.is_student}")
+
+    def serialize(self, filename):
+        with open(filename, "wb+") as f:
+            pickle.dump(self, f)
+
+    @classmethod
+    def deserialize(cls, filename):
+        output = None
+        with open(filename, "rb") as f:
+            output = pickle.load(f)
+        return output
