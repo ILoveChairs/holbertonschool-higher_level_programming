3f097192256d6b7e64a94d930815d3609c82dba6
Matias Davezac <8675@holbertonstudents.com>
2024-05-29 10:55:33 -0300

    Added T6

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python3
 
 '''
-	quick doc
+    quick doc
 '''
+
+import json
+from io import StringIO
+
+
+def load_from_json_file(filename):
+    '''
+        quick doc
+    '''
+
+    io = None
+    with open(filename) as f:
+        io = StringIO(json.load(f.read()))
+    return io
