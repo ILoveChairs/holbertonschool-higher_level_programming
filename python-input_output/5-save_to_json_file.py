f6b730b31a8de9b567d411dd7b9baec4d3ecd997
Matias Davezac <8675@holbertonstudents.com>
2024-05-29 10:49:06 -0300

    Added T5

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
 
 '''
-	quick doc
+    quick doc
 '''
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    '''
+        quick doc
+    '''
+
+    with open(filename, "w") as f:
+        f.write(json.dumps(my_obj))
