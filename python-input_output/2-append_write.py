b6d177864b208c403f0e3e8ab2eded46ab4e6141
Matias Davezac <8675@holbertonstudents.com>
2024-05-29 10:18:23 -0300

    Added T2

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
 
 '''
-	quick doc
+    quick doc
 '''
+
+def append_write(filename="", text=""):
+    '''
+        quick doc
+    '''
+
+    n_written = 0
+    with open(filename, "a+") as f:
+        n_written = f.write(text)
+    return n_written
