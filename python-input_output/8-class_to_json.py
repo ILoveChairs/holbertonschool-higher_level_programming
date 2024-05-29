3551560a236f38290511a5d72a3dff1e35528d43
Matias Davezac <8675@holbertonstudents.com>
2024-05-29 11:25:31 -0300

    Added T8

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
 
 '''
-	quick doc
+    quick doc
 '''
+
+import json
+
+
+def class_to_json(obj):
+    '''
+        quick doc
+    '''
+
+    return json.dumps(obj, default=lambda o: o.__dict__)
