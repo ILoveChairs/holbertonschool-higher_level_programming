e3c7c7b934b8a1ce5c682c188a8b45d5d1d8aef9
Matias Davezac <8675@holbertonstudents.com>
2024-05-29 11:47:01 -0300

    Added T9

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,5 +1,23 @@
 #!/usr/bin/python3
 
 '''
-	quick doc
+    quick doc
 '''
+
+
+class Student():
+    '''
+        quick doc
+    '''
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        return {
+            key: getattr(self, key)
+            for key in dir(self)
+            if key[:2] != "__"
+            }
