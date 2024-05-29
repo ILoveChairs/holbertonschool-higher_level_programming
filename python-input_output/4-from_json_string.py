ac4bde86ed0fd8c4ef45e294fdd42855af7aee48
Matias Davezac <8675@holbertonstudents.com>
2024-05-29 10:44:07 -0300

    Added T4

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
 
 '''
-	quick doc
+    quick doc
 '''
+
+from io import StringIO
+import json
+
+
+def from_json_string(my_str):
+    '''
+        quick doc
+    '''
+
+    io = StringIO(my_str)
+    return json.load(io)
