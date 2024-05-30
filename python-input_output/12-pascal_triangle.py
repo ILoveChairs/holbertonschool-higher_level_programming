d323f5500e8581c4a4af517ea758e1e900a571c1
Matias Davezac <8675@holbertonstudents.com>
2024-05-30 15:00:33 -0300

    Forgor 12

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,5 +1,25 @@
 #!/usr/bin/python3
 
 '''
-	quick doc
+    quick doc
 '''
+
+
+def pascal_triangle(n):
+    '''
+        quick doc
+    '''
+
+    if n <= 0:
+        return []
+
+    superlist = []
+    for num in range(n):
+        lst = [1]
+
+        for i in range(1, num + 1):
+            lst.append(i)
+
+        superlist.append(lst)
+
+    return superlist
