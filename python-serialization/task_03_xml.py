4bce89d1a02df57edf39776bfeb2352efbf03552
Matias Davezac <matiasdavezac@gmail.com>
2024-06-02 17:28:14 -0300

    Fixed T3

--- python-serialization/task_03_xml.py
+++ python-serialization/task_03_xml.py
@@ -8,13 +8,12 @@ import xml.etree.ElementTree as ET
 
 
 def adder_recursive(element, parent):
+    new = ET.SubElement(parent, element)
     if isinstance(element, dict):
         for sub_element in element:
-            new = ET.SubElement(parent, sub_element)
-            if isinstance(sub_element, dict):
-                adder_recursive(sub_element, new)
-            else:
-                new.text = element[sub_element]
+            adder_recursive(sub_element, new)
+    else:
+        new.text = parent[element]
 
 
 def serialize_to_xml(dictionary, filename):
@@ -23,7 +22,8 @@ def serialize_to_xml(dictionary, filename):
     '''
 
     root = ET.Element('data')
-    adder_recursive(dictionary, root)
+    for element in root:
+        adder_recursive(element, root)
     tree = ET.ElementTree(root)
     tree.write(filename)
 
