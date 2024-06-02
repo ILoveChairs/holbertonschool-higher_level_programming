9fe8b9ffd96455d810005b778f517badc8edf942
Matias Davezac <matiasdavezac@gmail.com>
2024-06-02 20:01:44 -0300

    Fixed T2, now prints status code correctly

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -14,7 +14,7 @@ def fetch_and_print_posts():
     '''
 
     r = requests.get('https://jsonplaceholder.typicode.com/posts')
-    print(r.status_code)
+    print(f"Status Code: {r.status_code}")
     dic = r.json()
     for sub_dic in dic:
         print(sub_dic['title'])
