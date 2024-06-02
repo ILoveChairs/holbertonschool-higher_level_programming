0f4693a0d6d38d2c4e36f761959fae2116908273
Matias Davezac <matiasdavezac@gmail.com>
2024-06-02 18:05:40 -0300

    Fix T3, final

--- python-serialization/task_03_xml.py
+++ python-serialization/task_03_xml.py
@@ -7,13 +7,13 @@
 import xml.etree.ElementTree as ET
 
 
-def adder_recursive(element, parent):
-    new = ET.SubElement(parent, element)
-    if isinstance(element, dict):
-        for sub_element in element:
-            adder_recursive(sub_element, new)
+def adder_recursive(key, parent_dict, parent_element):
+    new = ET.SubElement(parent_element, key)
+    if isinstance(parent_dict[key], dict):
+        for sub_key in parent_dict[key]:
+            adder_recursive(sub_key, parent_dict[key], new)
     else:
-        new.text = parent[element]
+        new.text = parent_dict[key]
 
 
 def serialize_to_xml(dictionary, filename):
@@ -22,9 +22,10 @@ def serialize_to_xml(dictionary, filename):
     '''
 
     root = ET.Element('data')
-    for element in root:
-        adder_recursive(element, root)
+    for element in dictionary:
+        adder_recursive(element, dictionary, root)
     tree = ET.ElementTree(root)
+    ET.indent(tree)
     tree.write(filename)
 
 
@@ -32,7 +33,11 @@ def reader_recursive(element):
     if len(element):
         new_dict = {}
         for sub_element in element:
-            new_dict[sub_element.tag] = reader_recursive(sub_element).text
+            content = reader_recursive(sub_element)
+            if not len(content):
+                new_dict[sub_element.tag] = reader_recursive(sub_element).text
+            else:
+                new_dict[sub_element.tag] = reader_recursive(sub_element)
         return new_dict
     else:
         return element
