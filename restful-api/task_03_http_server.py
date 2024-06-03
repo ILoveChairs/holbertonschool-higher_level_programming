a2454a545b4d2b8e643efe0e63d54052a0205530
Matias Davezac <matiasdavezac@gmail.com>
2024-06-02 21:48:22 -0300

    Fixed T3, idk

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -46,7 +46,7 @@ class WebRequestHandler(HS.BaseHTTPRequestHandler):
             self.send_response(404)
             self.send_header('Content-type', 'text/html')
             self.end_headers()
-            self.wfile.write("endpoint not found".encode("utf-8"))
+            self.wfile.write("404 Not Found".encode("utf-8"))
 
 
 if __name__ == "__main__":
