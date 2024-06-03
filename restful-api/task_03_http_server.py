bf2dc1c5f0efbac297efa8f8abfb33f11577d30a
Matias Davezac <matiasdavezac@gmail.com>
2024-06-02 21:13:41 -0300

    Added T3, aaaaahhhhh

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+
+'''
+    quickdoc
+'''
+
+import http.server as HS
+import socketserver
+import json
+
+
+PORT = 8000
+
+class WebRequestHandler(HS.BaseHTTPRequestHandler):
+    '''
+        quick doc
+    '''
+
+    def send_text(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+
+    def do_GET(self):
+        '''
+            quick doc
+        '''
+        if self.path[-5:] == "/data":
+            self.send_response(200)
+            self.send_header("Content-Type", "application/json")
+            self.end_headers()
+            self.wfile.write(json.dumps({"name": "John", "age": 30, "city": "New York"}).encode("utf-8"))
+        elif self.path[-7:] == "/status":
+            self.send_text()
+            self.wfile.write("OK".encode("utf-8"))
+        elif self.path[-1:] == '/':
+            self.send_text()
+            self.wfile.write("Hello, this is a simple API!".encode("utf-8"))
+        else:
+            self.send_response(404)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write("Page not found".encode("utf-8"))
+
+
+if __name__ == "__main__":
+    Handler = WebRequestHandler
+
+    with socketserver.TCPServer(("127.0.0.1", PORT), Handler) as httpd:
+        httpd.serve_forever()
