dd518e2feb6805d38ac66814d8b5938285ab83d1
Matias Davezac <8675@holbertonstudents.com>
2024-06-23 18:15:15 +0000

    New task added

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,7 +1,7 @@
 -- Quick comment\n
 SELECT 
-    DISTINCT score, 
-    COUNT(*) AS "Count"
+        DISTINCT score, 
+        COUNT(score) AS "number"
     FROM second_table
     GROUP BY score
     ORDER BY score DESC;
