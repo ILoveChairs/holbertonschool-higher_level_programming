f033ce77fe2ce264a1a16517bfd5b7214e4e9395
Matias Davezac <8675@holbertonstudents.com>
2024-06-24 13:12:51 +0000

    idk

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,6 @@
 -- La Mona China
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-LEFT JOIN tv_show_genres
+INNER JOIN tv_show_genres
 ON tv_shows.genre_id = tv_show_genres.genre_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
