df976928c08f929cd84168250027ebcb4c6231d8
Matias Davezac <8675@holbertonstudents.com>
2024-06-24 18:17:30 -0300

    Added T2

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+
+'''
+    Testing Databases with MySQLdb (python)
+'''
+
+from sys import argv
+from typing import Any
+# import sqlalchemy
+import MySQLdb
+from MySQLdb import cursors
+
+
+def makeConnection(
+    argv: list[str]
+) -> MySQLdb.Connection:
+    '''
+        Make a connection with the database and returns it.
+    '''
+
+    db: MySQLdb.Connection = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
+
+    return db
+
+
+def makeAndPrintQuery(
+    cursor: cursors.Cursor,
+    query: str
+) -> None:
+    '''
+        Makes query and prints result as a tuple.
+    '''
+
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+
+def runQueries(
+    db: MySQLdb.Connection,
+    argv: str
+) -> None:
+    '''
+        Part that defines all queries to run.
+    '''
+
+    cursor = db.cursor()
+
+    query = '''SELECT * FROM states
+               WHERE BINARY name = '{}'
+               ORDER BY id ASC'''.format(argv[4])
+    makeAndPrintQuery(cursor, query)
+
+    cursor.close()
+
+
+def main() -> None:
+    '''
+        Calls all functions
+    '''
+
+    db = makeConnection(argv)
+
+    runQueries(db, argv)
+
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
