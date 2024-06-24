4b95bdc500dcbd49e71d15928e78e45d90961e5d
Matias Davezac <8675@holbertonstudents.com>
2024-06-24 15:03:59 +0000

    Fixed T16

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- La Mona China
-SELECT DISTINCT A.title, C.name
+SELECT A.title, C.name
 FROM tv_shows A
 LEFT JOIN tv_show_genres B ON A.id = B.show_id
 LEFT JOIN tv_genres C ON C.id = B.genre_id
