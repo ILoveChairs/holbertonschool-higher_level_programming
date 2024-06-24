d033f5fafc310866fca504b14b6eec5caffe329b
Matias Davezac <8675@holbertonstudents.com>
2024-06-24 13:30:42 -0300

    Initialized T0

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+'''
+    Testing Databases with MySQLdb (python)
+'''
+
+import sys
+import sqlalchemy
+#import MySQLdb
+
+
+def algo() -> None:
+    '''
+        Quickdoc
+    '''
+
+    pass
+
+
+def main(argv) -> None:
+    '''
+        Calls all functions
+    '''
+
+    algo()
+
+
+if __name__ == "__main__":
+    main(sys.argv)
