2f9e357b69759063df3ce96967ece3451ccdbaea
Matias Davezac <8675@holbertonstudents.com>
2024-06-24 12:51:32 +0000

    Fixed T9, put countries instead of states

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,4 @@
 -- Hail carrot
 SELECT cities.id, cities.name, states.name
-FROM cities A
-LEFT JOIN countries B
-ON A.country_id = B.id;
+FROM cities
+LEFT JOIN states ON cities.country_id = states.id;
