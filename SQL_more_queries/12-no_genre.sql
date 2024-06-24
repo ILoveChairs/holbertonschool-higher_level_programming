95f9700bbbc164f6adf0a282d665c3213acdb4dc
Matias Davezac <8675@holbertonstudents.com>
2024-06-24 13:22:04 +0000

    Did T12 instead of T11

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,7 @@
 -- La Mona China
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-LEFT JOIN tv_show_genres
+INNER JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC
+WHERE tv_shows.id IS NOT NULL;
