69237030072ee728e6c1124a4e55cd6b39844c73
Matias Davezac <8675@holbertonstudents.com>
2024-06-24 14:32:15 -0300

    Added T0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -5,34 +5,55 @@
 '''
 
 import sys
+from typing import Any
 import sqlalchemy
 import MySQLdb
 
 
-def makeConnection(argv) -> :
+def makeConnection(argv: list[str]
+                   ) -> tuple[MySQLdb.Connection,
+                              MySQLdb.cursors.Cursor]:
     '''
-        Quickdoc
+        Make a connection with the database and return
+        the connection and a cursor.
+
+        Both must be closed later.
     '''
 
-    db = 
-    cursor = 
+    db: MySQLdb.Connection
+    db = MySQLdb.connect(host="localhost",
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+    cursor: MySQLdb.cursors.Cursor
+    cursor = db.cursor(cursorclass=MySQLdb.cursors.Cursor)
     return db, cursor
 
 
-def makeQuery(cursor, query, printAsTuple=True) -> tuple:
+def makeQuery(cursor: MySQLdb.cursors.Cursor,
+              query: str
+              ) -> int:
     '''
         Makes a query to the database by the cursor arg.
+
+        Execute apparently does it's things inside it's class.
+        Also returns number of rows affected?
     '''
 
-    pass
+    return cursor.execute(query)
 
 
-def makeAndPrintQuery(cursor, query) -> None:
+def makeAndPrintQuery(cursor: MySQLdb.cursors.Cursor,
+                      query: str
+                      ) -> None:
     '''
         Calls makeQuery and prints result as a tuple.
     '''
 
-    pass
+    nOfRows: int = makeQuery(cursor, query)
+    rows: tuple[tuple[Any]] = cursor.fetchall()
+    for row in rows:
+        print(row)
 
 
 def main(argv) -> None:
@@ -42,7 +63,8 @@ def main(argv) -> None:
 
     db, cursor = makeConnection(argv)
 
-    makeAndPrintQuery(db, cursor, "")
+    query = "SELECT * FROM states ORDER BY states.id ASC;"
+    makeAndPrintQuery(db, cursor, query)
 
     db.close()
     cursor.close()
