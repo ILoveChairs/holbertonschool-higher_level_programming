5012fad25826a8aeac74e0879ff51246c40101cb
Matias Davezac <8675@holbertonstudents.com>
2024-06-25 10:05:48 -0300

    Fixed T4

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -68,7 +68,7 @@ def user_adder():
             or dic["username"].isspace():
         return {"error": "Username is required"}, 400
     if dic["username"] in users:
-        return {"error": "Conflict"}, 409
+        return {"error": "Username is required"}, 400
     users[dic["username"]] = dic
     return {"message": "User added", "user": dic}, 201
 
