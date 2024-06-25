b67e17bbac85c97df1e6da3d67928b11c225cd05
Matias Davezac <8675@holbertonstudents.com>
2024-06-24 22:57:50 -0300

    Fixed T11

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -19,4 +19,5 @@ if __name__ == "__main__":
     session.add(newState)
     state = session.query(State).filter(State.name == name).first()
     print(state.id)
+    session.commit()
     session.close()
