5fee4eab9a827e2fd17246bd6c46c00f0682292d
Matias Davezac <8675@holbertonstudents.com>
2024-07-15 13:22:36 -0300

    Added T1

--- python-server_side_rendering/task_01_jinja.py
+++ python-server_side_rendering/task_01_jinja.py
@@ -4,3 +4,35 @@
     quickdoc
 '''
 
+from flask import Flask, render_template
+from jinja2 import Environment, FileSystemLoader
+
+# Instanciates the flask app
+app = Flask(__name__)
+
+# Get's environment??? also sets template folder as templates
+env = Environment(loader=FileSystemLoader('templates'))
+
+# Gets and renders all templates
+home_template = env.get_template('index.html').render()
+about_template = env.get_template('about.html').render()
+contact_template = env.get_template('contact.html').render()
+
+
+@app.route('/')
+def home():
+    return home_template, 200
+
+
+@app.route('/about')
+def about():
+    return about_template, 200
+
+
+@app.route('/contact')
+def contact():
+    return contact_template, 200
+
+
+if __name__ == '__main__':
+    app.run(port=5000)
