095fd975b3063ac09330b088328d5379f18ba6d8
Matias Davezac <8675@holbertonstudents.com>
2024-08-13 13:54:59 -0300

    Fixed T101 pep8

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -5,12 +5,11 @@
 '''
 
 
-
 class LockedClass():
     ''' quickdoc '''
 
     def __setattr__(self, key, value):
         if key != 'first_name':
             raise AttributeError(
-                f"'LockedClass' object has no attribute '{key}'")
+                f"'{self.__name__}' object has no attribute '{key}'")
         object.__setattr__(self, key, value)
