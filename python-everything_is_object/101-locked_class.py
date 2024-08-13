482ffe81622bba93267f629b2724b837849a0212
Matias Davezac <8675@holbertonstudents.com>
2024-08-13 13:57:39 -0300

    Fixed T101, key is not attr

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -12,4 +12,4 @@ class LockedClass():
         if key != 'first_name':
             raise AttributeError(
                 f"'{self.__class__.__name__}' object has no attribute '{key}'")
-        self.key = value
+        self.first_name = value
