e4e1c31dd7ba9630af3f1bb1a0e5125a30203ae0
Matias Davezac <8675@holbertonstudents.com>
2024-08-13 13:39:52 -0300

    Fixed T100, pep8

--- python-everything_is_object/100-magic_string.py
+++ python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string() -> str:
-    globals()['c'] = globals()['c'] + 1 if globals().get('c') is not None else 0
+    globals()['c'] = 0 if globals().get('c') is None else globals()['c'] + 1
     return globals()['c'] * 'BestSchool, ' + 'BestSchool'
