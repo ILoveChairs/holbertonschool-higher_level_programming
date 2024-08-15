3ceb059460c8dade79130b89a7f1362012985387
Matias Davezac <8675@holbertonstudents.com>
2024-08-15 09:29:42 -0300

    Fixed T101

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -9,10 +9,8 @@ class MetaClass:
     ''' quickdoc '''
 
     def __setattr__(self, key, value):
-        if key != 'first_name':
-            raise AttributeError(
-                f"'{self.__class__.__name__}' object has no attribute '{key}'")
-        self.__dict__['first_name'] = value
+        raise AttributeError(
+            f"'{self.__class__.__name__}' object has no attribute '{key}'")
 
 
 class LockedClass(MetaClass):
